93b5eeddc859dc12666497c4877a19ce78b841e7
Aaron Klinker <aaronklinker1@gmail.com>
2019-10-06 15:30:42 -0500

    WIP

--- pkg/mutations.go
+++ pkg/mutations.go
@@ -1,18 +1,3 @@
 package pkg
 
 func updateModel()
-
-// mutation UpdatePreferences($id: Int, $data: Input_Preferences) {
-//     updatePreferences(_id: $id, data: $data) {
-//         _id
-//         theme
-// #         user {
-// #             _id
-// #             email
-// #             firstName
-// #             fullName
-// #             username
-// #             validation
-// #         }
-//     }
-// }
